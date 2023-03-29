dfe5e0764ab52c2c2db698a99e95e1c723039ee9
vennela vallabhaneni <128285228+vennby@users.noreply.github.com>
2023-03-29 16:39:32 +0400

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -18,3 +18,26 @@ int main()
 	
 	return 0;
 }	
+
+An alteration of this code includes printing the factorial values of all the numbers that lead upto the user-input value. This can be done by simply placing the final 'printf' command inside the first loop, and by replacing 'n' with 'i'.
+
+#include <stdio.h>
+
+int main()
+{
+	int n, i, j, f;
+	printf("\n Enter the value of 'n': ");
+	scanf("%d", &n);
+	
+	for(i=1;i<=n;i++)
+	{
+		f=1;
+		for(j=1;j<=i;j++)
+		{
+			f=f*j;
+		}
+		printf("\n The factorial value of %d is %d", i, f);
+	}
+	
+	return 0;
+}
