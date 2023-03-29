1721a0caac94a663ce774950df344964e341dfbd
vennela vallabhaneni <128285228+vennby@users.noreply.github.com>
2023-03-29 18:20:10 +0400

    Update farecalculation.c

--- farecalculation.c
+++ farecalculation.c
@@ -2,13 +2,18 @@
 
 int main()
 {
+    float b, m, c;
+    
+    printf("\n Enter the cost per km for bus, metro, and cab: ");
+    scanf("%f%f%f", &b, &m, &c);
+    
     int days, i, mode;
     
     printf("\n Enter the number of days: ");
     scanf("%d", &days);
     
-    printf("\n Enter 1 for bus, 2 for metro, 3 for cab.");
-    printf("\n");
+    printf("\n Enter 1 for bus, 2 for metro, 3 for cab: ");
+    scanf("%d", %mode);
    
     float distance, sum=0;
     
@@ -20,9 +25,9 @@ int main()
         scanf("%f", &distance);
         switch(mode)
         {
-            case 1: sum=sum+(3*distance); break;
-            case 2: sum=sum+(5*distance); break;
-            case 3: sum=sum+(9*distance); break;
+            case 1: sum=sum+(b*distance); break;
+            case 2: sum=sum+(m*distance); break;
+            case 3: sum=sum+(c*distance); break;
             default: printf("\n Enter one of the three mentioned modes of transport.");
         }
     }
