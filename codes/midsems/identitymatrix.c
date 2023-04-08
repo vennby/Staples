493fc05b28a4f0c513e9f0e5ae5379ada9d3af88
vennela vallabhaneni <128285228+vennby@users.noreply.github.com>
2023-04-08 10:41:06 +0400

    Create identitymatrix.c

---
+++ codes/midsems/identitymatrix.c
@@ -0,0 +1,70 @@
+// Q: Write a program in C to check whether a given matrix is an identity matrix or not.
+
+#include <stdio.h>
+
+int main()
+{
+    int r, c;
+    
+    printf("\n Input number of rows for the matrix: ");
+    scanf("%d", &r);
+    
+    printf("\n Input number of columns for the matrix: ");
+    scanf("%d", &c);
+    
+    int a[r][c], i, j;
+    
+    printf("\n Input elements in the first matrix: ");
+    
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("\n element - [%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }    
+    }
+    
+    printf("\n The matrix is: ");
+    
+    for(i=0;i<r;i++)
+    {
+        printf("\n");
+        for(j=0;j<c;j++)
+        {
+            printf("%d \t", a[i][j]);
+        }    
+    }
+    
+    int flag=1;
+    
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i==j)
+            {
+                if(a[i][j]!=1)
+                {
+                    flag=0;
+                    break;
+                }   
+            }
+            else
+            {
+                if (a[i][j]!=0)
+                {
+                    flag=0;
+                    break;
+                }
+            }
+        }
+    }
+    
+    if (flag==0)
+    printf("\n The matrix is not an identity matrix.");
+    else
+    printf("\n The matrix is an identity matrix.");
+    
+    return 0;
+}
