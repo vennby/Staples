28746d57025c1739aeb85c1c6aa71d7ab4b353ab
vennela vallabhaneni <128285228+vennby@users.noreply.github.com>
2023-04-25 20:35:18 +0400

    Create storingrootscubes.c

---
+++ codes/general/storingrootscubes.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int upp;
+    
+    printf("\n Enter an upper bound: ");
+    scanf("%d", &upp);
+    
+    int n, i, count=0;
+    
+    for(n=1;n<upp;n++)
+    {
+        for(i=1;i<=n;i++)
+        {
+            if(n==i*i)
+            {
+                count++;
+                break;
+            }
+        }
+    }
+    
+    printf("\n There are %d integers between 1 to %d that have a perfect square root.", count, upp);
+    
+    int roots[count], cubes[count], number[count], j, k;
+    
+    for(n=1;n<upp;n++)
+    {
+        for(i=1,j=0;i<=n,j<count;i++,j++)
+        {
+            if(n==i*i)
+            {
+                number[j]=n;
+                roots[j]=i;
+                cubes[j]=n*n*n;
+                break;
+            }
+        }
+    }
+    
+    printf("\n");
+    
+    printf("\n These integers are:");
+    
+    for(j=0,k=1;j<=count,k<=count;j++,k++)
+    {
+        printf("\n %d. %d", k, number[j]);
+    }    
+    
+    printf("\n");
+    
+    printf("\n The square roots of these integers are:");
+    
+    for(j=0,k=1;j<=count,k<=count;j++,k++)
+    {
+        printf("\n %d. %d", k, roots[j]);
+    }
+    
+    printf("\n");
+    
+    printf("\n The cubes of these integers are:");
+    
+    for(j=0,k=1;j<=count,k<=count;j++,k++)
+    {
+        printf("\n %d. %d", k, cubes[j]);
+    }
+
+    return 0;
+}
